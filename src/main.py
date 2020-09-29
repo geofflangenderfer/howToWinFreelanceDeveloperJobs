df79a5cbcfb29c34b700b55a7c26d38f605cb501
geoff langenderfer <geoff.langenderfer>
2020-09-29 17:02:02 -0500

    main.py should be ready to be run independently

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 #from top_emojis.CalculateEmojis import top_emojis.CalculateEmojis.CalculateEmojis
-import top_emojis.CalculateEmojis
+from TwitterSearch import TwitterSearch
+from SaveTweets import SaveTweets
+from CalculateEmojis import CalculateEmojis
+from SaveNumberOfEmojis import SaveNumberOfEmojis
 
 if __name__ == '__main__':
-    test = top_emojis.CalculateEmojis.CalculateEmojis()
-    for tweet in test.tweets:
-        print(tweet)
+    authors = ['paulg', 'r00k', 'brennandunn', 'dvassallo', 'naval']
+    for author in authors:
+        search = TwitterSearch(author)
+        SaveTweets(search.tweets)
+
+    emojis = CalculateEmojis()
+    SaveNumberOfEmojis(emojis)
+
+
 
